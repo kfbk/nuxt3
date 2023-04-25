69946f2523f80a2df7de7c545a453ace537a096d
DESKTOP-DC91P5K\user <satou1497@gmail.com>
2023-04-25 09:37:14 +0900

    4th

--- components/postList.vue
+++ components/postList.vue
@@ -11,15 +11,13 @@ const { posts } = defineProps<Props>()
 <template>
   <!-- <article v-for="post in posts.contents" :key="post.id"> -->
   <article v-for="post in posts" :key="post.id">
-    <div v-if="post">
+    <!-- <div v-if="post">
     <span>
-      <!-- 次を日付用プラグインを呼び出すように変更（ここだけの変更で済む）
-         {{ post.publishedAt }} -->
          {{ $formatDate(String(post.publishedAt)) }}
     </span>
     <NuxtLink :to="`/${post.id}`">
       <h1>{{ post.title }}</h1>
     </NuxtLink>
-    </div>
+    </div> -->
   </article>
 </template>
